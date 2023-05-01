c26400221de08e247f58a4bd99005025a3b9e103
hey <103824094+mushroomhater07@users.noreply.github.com>
2023-05-01 01:31:21 -0700

    Add files via upload

---
+++ Csharp/ReadText.cs
@@ -0,0 +1,12 @@
+﻿
+static string GetText(string path , string filename)
+    {
+    var reader = File.OpenText($"{AppendPathSeparator(path)}{filename}");
+    var text = reader.ReadToEnd();
+    return text;
+
+    string AppendPathSeparator(string filepath)
+        {
+        return filepath.EndsWith(@"\") ? filepath : filepath + @"\";
+        }
+    }
